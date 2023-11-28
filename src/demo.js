c3f32e3566e480d9d2fbd07de236864325160fa6
now17 <now17@sgrsoft.com>
2023-11-28 12:44:51 +0900

    Initial commit

---
+++ src/demo.js
@@ -0,0 +1,36 @@
+import ncpDrm from './ncpDrm.js'
+
+let contentId = 'VIDEO24'; //컨텐츠 아이디 대입
+let userId = 'test-user';
+
+//DRM Helper
+const NDRM = new ncpDrm(userId);
+// 재생소스 구성
+let drmSource = NDRM.drmSourceHelper({
+    dash : 'https://jdybvoww1713.edge.naverncp.com/live/video/ls-20231127114441-qcwdD/live.mpd',
+    hls : 'https://jdybvoww1713.edge.naverncp.com/live/video/ls-20231127114441-qcwdD/playlist.m3u8',
+},contentId);
+
+
+
+window.player = null;
+document.addEventListener('DOMContentLoaded', async () => {
+
+    window.drmPlayer = await ncplayerDRM('player',{
+        playlist:[
+            {
+                drm:drmSource,
+                description:{
+                    title:"DRM 테스트",
+                    profile_name:"네이버클라우드",
+                    profile_image:"https://nnbkegvqsbcu5297614.cdn.ntruss.com/profile/202208/d127c8db642716d84b3201f1d152e52a.png",
+                }
+            },
+        ],
+        autostart:true,
+        muted:true,
+        progressBarColor:"#ff0000",
+    });
+
+    document.querySelector('.vpe-drm-state').innerHTML = await checkDRM();
+});
